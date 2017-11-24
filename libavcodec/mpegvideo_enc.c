3f5922b605c715e4a92b2e1465777ca17cf0cb5d
gogan <gogan@bigmir.net>
2017-11-24 12:30:31 +0200

    fixed for qscale=1
    Signed-off-by: gogan <gogan@bigmir.net>

--- libavcodec/mpegvideo_enc.c
+++ libavcodec/mpegvideo_enc.c
@@ -376,19 +376,23 @@ FF_ENABLE_DEPRECATION_WARNINGS
     s->avctx        = avctx;
 
     // forcing max rate and buffer size
-    if(avctx->rc_max_rate || avctx->rc_buffer_size){
-        av_log(avctx, AV_LOG_WARNING, "FrontPictures special edittion: max rate and buffer size ignored\n");
-    }
-    if(avctx->bit_rate_tolerance!=200*1000*20){
-        av_log(avctx, AV_LOG_WARNING, "FrontPictures special edittion: bitrate tolerance ignored\n");
-    }
-    if(avctx->time_base.den>0 && avctx->time_base.num>0 && avctx->bit_rate){
-        avctx->rc_max_rate=avctx->bit_rate*2;
-        avctx->rc_buffer_size=(int)(avctx->bit_rate*4*(int64_t)avctx->time_base.num/(int64_t)avctx->time_base.den);
-        avctx->bit_rate_tolerance=avctx->bit_rate;
-        avctx->rc_initial_buffer_occupancy = avctx->rc_buffer_size * 3LL / 4;
-        av_log(avctx, AV_LOG_INFO, "FrontPictures special edittion: forcing max rate=%0.2fM, buf size=%0.2fM, bitrate tolerance=%0.2fM\n",
-               (float)avctx->rc_max_rate/1000000.0f,(float)avctx->rc_buffer_size/1000000.0f,(float)avctx->bit_rate_tolerance/1000000.0f);
+    if(!(avctx->flags & AV_CODEC_FLAG_QSCALE)){
+        if(avctx->rc_max_rate || avctx->rc_buffer_size){
+            av_log(avctx, AV_LOG_WARNING, "FrontPictures special edittion: max rate and buffer size ignored\n");
+        }
+        if(avctx->bit_rate_tolerance!=200*1000*20){
+            av_log(avctx, AV_LOG_WARNING, "FrontPictures special edittion: bitrate tolerance ignored\n");
+        }
+        if(avctx->time_base.den>0 && avctx->time_base.num>0 && avctx->bit_rate){
+            avctx->rc_max_rate=avctx->bit_rate*2;
+            avctx->rc_buffer_size=(int)(avctx->bit_rate*4*(int64_t)avctx->time_base.num/(int64_t)avctx->time_base.den);
+            avctx->bit_rate_tolerance=avctx->bit_rate;
+            avctx->rc_initial_buffer_occupancy = avctx->rc_buffer_size * 3LL / 4;
+            av_log(avctx, AV_LOG_INFO, "FrontPictures special edittion: forcing max rate=%0.2fM, buf size=%0.2fM, bitrate tolerance=%0.2fM\n",
+                   (float)avctx->rc_max_rate/1000000.0f,(float)avctx->rc_buffer_size/1000000.0f,(float)avctx->bit_rate_tolerance/1000000.0f);
+        }
+    }else{
+        av_log(avctx, AV_LOG_WARNING, "FrontPictures special edittion: not forcing anything for fixed qscale\n");
     }
 
     if (avctx->max_b_frames > MAX_B_FRAMES) {
